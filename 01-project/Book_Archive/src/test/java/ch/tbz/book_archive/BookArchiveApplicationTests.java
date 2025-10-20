4320e2bfcf6bb905d5e5cd8812f046440edd8110
Luka Grujic <luka.grujic@edu.tbz.ch>
2025-10-20 10:54:09 +0200

    project setup

---
+++ 01-project/Book_Archive/src/test/java/ch/tbz/book_archive/BookArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.tbz.book_archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
