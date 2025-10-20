8f805d6c4924f53c10f9514ad5da0dd027b2ebe2
Luka Grujic <luka.grujic@edu.tbz.ch>
2025-10-20 11:24:01 +0200

    generic classes create

---
+++ 01-project/Book_Archive/src/main/java/ch/tbz/book_archive/core/generic/AbstractDTO.java
@@ -0,0 +1,16 @@
+package ch.tbz.book_archive.core.generic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public abstract class AbstractDTO {
+  private UUID id;
+}
---
+++ 01-project/Book_Archive/src/main/java/ch/tbz/book_archive/core/generic/AbstractEntity.java
@@ -0,0 +1,26 @@
+package ch.tbz.book_archive.core.generic;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractEntity {
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(columnDefinition = "uuid", name = "id", updatable = false, nullable = false)
+  private UUID id;
+}
