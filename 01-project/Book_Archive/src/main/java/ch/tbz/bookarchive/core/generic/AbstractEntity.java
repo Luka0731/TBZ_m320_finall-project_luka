5bf7477c44962d199cfc0983bf33a5607d6a9fa5
Luka Grujic <luka.grujic@edu.tbz.ch>
2025-10-25 22:44:36 +0200

    logging to abstract entity add

--- 01-project/Book_Archive/src/main/java/ch/tbz/bookarchive/core/generic/AbstractEntity.java
+++ 01-project/Book_Archive/src/main/java/ch/tbz/bookarchive/core/generic/AbstractEntity.java
@@ -1,13 +1,11 @@
 package ch.tbz.bookarchive.core.generic;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
@@ -17,10 +15,26 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Log4j2
 public abstract class AbstractEntity {
   @Id
   @GeneratedValue(generator = "uuid2")
   @GenericGenerator(name = "uuid2", strategy = "uuid2")
   @Column(columnDefinition = "uuid", name = "id", updatable = false, nullable = false)
   private UUID id;
+
+  @PostPersist
+  protected void onCreate() {
+      log.info("Created new entity of type: {}", this.getClass().getSimpleName());
+  }
+
+  @PostUpdate
+  protected void onUpdate() {
+      log.info("Updated entity of type: {} with ID {}", this.getClass().getSimpleName(), id);
+  }
+
+  @PostRemove
+  protected void onDelete() {
+      log.info("Deleted entity of type: {} with ID {}", this.getClass().getSimpleName(), id);
+  }
 }
